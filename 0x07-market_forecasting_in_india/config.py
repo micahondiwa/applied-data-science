66c5c99f4c5b0f9365dd181e27d611aee990f56e
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-06-07 03:30:02 +0300

    GarchModel Class: Wrangle_method

--- 0x07-market_forecasting_in_india/config.py
+++ 0x07-market_forecasting_in_india/config.py
@@ -1,4 +1,4 @@
-"""This module extracts information from `.env` file so that
+"""This module extracts information from .env file so that
 we can use AplhaVantage API key in other parts of the application.
 """
 
